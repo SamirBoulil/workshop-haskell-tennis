35846edcbd5f3220f9bb0092e56bc3064a0e1b5c
Samir Boulil <samir.boulil@akeneo.com>
2018-05-07 11:43:47 +0200

    Calculating winning strikes with types and recursion

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,26 +5,35 @@ main = hspec $ do
 
   describe "The score of a tennis point" $ do
     it "returns 0 - 0 when there was no winning strike from each part of the players" $
-      score [] `shouldBe` (0, 0)
+      score [] (Zero, Zero) `shouldBe` (Zero, Zero)
 
     it "returns 15-0 when player A makes a winning strike" $
-      score [A] `shouldBe` (15, 0)
+      score [A] (Zero, Zero) `shouldBe` (Quinze, Zero)
 
     it "returns 30-0 when player A makes two consecutive winning strikes" $
-      score [A, A] `shouldBe` (30, 0)
+      score [A, A] (Zero, Zero) `shouldBe` (Trente, Zero)
 
     it "returns 40-0 when player A makes three consecutive winning strikes" $
-      score [A, A, A] `shouldBe` (40, 0)
+      score [A, A, A] (Zero, Zero) `shouldBe` (Quarante, Zero)
 
     it "return 40-15 when Player A makes three winning strikes and Player B makes one winning strike" $
-      score [A, A, B, A] `shouldBe` (40, 15)
+      score [A, A, B, A] (Zero, Zero) `shouldBe` (Quarante, Quinze)
 
 data Player = A | B deriving (Show, Eq)
 
-score :: [Player] -> (Int, Int)
-score [] = (0, 0)
-score [A] = (15, 0)
-score [A, A] = (30, 0)
-score [A, A, A] = (40, 0)
-score [A, A, B, A] = (40, 15)
-score [A, B, A, B, A] = (40, 15)
+data Point = Zero | Quinze | Trente | Quarante deriving (Show, Eq, Enum)
+
+{-Pattern matching-}
+{-score :: [Player] -> (Int, Int)-}
+{-score [] = (0, 0)-}
+{-score [A] = (15, 0)-}
+{-score [A, A] = (30, 0)-}
+{-score [A, A, A] = (40, 0)-}
+{-score [A, A, B, A] = (40, 15)-}
+{-score [A, B, A, B, A] = (40, 15)-}
+
+{-Recursion + Pattern matching-}
+score :: [Player] -> (Point, Point) -> (Point, Point)
+score (A:tail) (scoreA, scoreB) = score tail (succ scoreA, scoreB)
+score (B:tail) (scoreA, scoreB) = score tail (scoreA, succ scoreB)
+score [] (scoreA, scoreB) = (scoreA, scoreB)
